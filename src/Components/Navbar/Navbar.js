2ee2e8c497c7197d7cd0bdf06ec1c66c78e62dd3
labibahmed10 <labib.ahmed.372@gmail.com>
2022-04-16 22:02:35 +0600

    navbar made responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../../images/logo.svg";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
   return (
-    <div className="flex items-center justify-between md:px-20 px-6 md:py-4 py-2 bg-[#F4FCFA] text-[1.1rem] font-semibold">
+    <div className="flex items-center justify-between md:px-20 px-6 md:py-4 py-3 bg-[#F4FCFA] text-[1.1rem] font-semibold relative">
       <img src={logo} alt="" />
-      <div className="sm:space-x-8 flex sm:flex-row flex-col">
+
+      <div onClick={() => setShowNav(!showNav)} className="sm:hidden cursor-pointer">
+        <GiHamburgerMenu></GiHamburgerMenu>
+      </div>
+
+      <div
+        className={`sm:space-x-8 flex sm:flex-row flex-col absolute  sm:static ${
+          showNav ? "block items-center top-12 right-0 duration-500 bg-[#F4FCFA] w-full p-4" : "top-[-300px]"
+        }`}
+      >
         <NavLink
           className={({ isActive }) =>
             isActive ? "text-[#26ABA3] underline underline-offset-2" : "text-slate-500"
