d01df36337298ff12e997345a6f58f0aeda52da9
labibahmed10 <labib.ahmed.372@gmail.com>
2022-04-17 17:53:31 +0600

    answered one question in blog section

--- src/Components/MyBlogs/MyBlogs.js
+++ src/Components/MyBlogs/MyBlogs.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const MyBlogs = () => {
   return (
-    <div className="sm:mt-20 mt-10 px-10">
-      <div className="text-justify">
+    <div className="mt-10 px-10">
+      <div className="text-justify sm:mt-16">
         <h1 className="text-center text-3xl font-semibold py-5 text-[#26ABA3]">
           Q1. Differences Between Authentication and Authorization
         </h1>
@@ -19,11 +19,35 @@ const MyBlogs = () => {
         </p>
       </div>
 
-      <div className="text-justify">
+      <div className="text-justify sm:mt-16">
         <h1 className="text-center text-3xl font-semibold py-5 text-[#26ABA3]">
-          {" "}
           Why are you using firebase? What other options do you have to implement authentication?
         </h1>
+        <p className="text-xl text-slate-500">
+          Firebase is platform provided by Google which is pretty simple to connect and use built-in
+          third-party authentication providers like Google,Facebook,Github,Twitter,Apple etc.Firebase
+          bascically takes users information and make connection between those google's or github's backend
+          server and as a result login the user to that application.In this proccess users important data's
+          like password are encrypted. <br />
+          There are some other options for authentication like <br />
+          1.Parse <br />
+          2. Back4App. <br />
+          3.Kinvey. etc
+        </p>
+      </div>
+      <div className="text-justify sm:mt-16 mb-10">
+        <h1 className="text-center text-3xl font-semibold py-5 text-[#26ABA3]">
+          What other services does firebase provide other than authentication?
+        </h1>
+        <p className="text-xl text-slate-500">
+          1.Backend Services, <br />
+          2.Easy-to-use SDKs, <br />
+          3.Ready Made UI Libraries. <br />
+          4.Hosting. <br />
+          5.Realtime Database. <br />
+          5.Cloud Firestore. <br />
+          6. Cloud Messaging.
+        </p>
       </div>
     </div>
   );
